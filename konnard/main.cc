a99cc1db0810d2b2c6950277e43a0da81450d5d6
Thomas Lasne <th.lasne@gmail.com>
2011-08-14 16:25:46 +0200

    mon premier prog

---
+++ konnard/main.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main ()
+
+{
+
+  for (int i = 0; i < 5; ++i)
+  {
+    std::cout << "Hello World" << std::endl
+	      << "Hello World" << std::endl;
+  }
+
+
+  return 0;
+
+}
